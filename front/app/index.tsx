753281d52357a9cec81ad535bd3c05c49f47d715
Frang84 <skorniak.skorniak@wp.pl>
2024-12-17 12:52:05 +0100

    login
    before changes in file structure.

---
+++ front/app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native"
+
+
+const LoginPage = () =>{
+    return(
+        <View>
+            <Text>
+                Login Page
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+});
