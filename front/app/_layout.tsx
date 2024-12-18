acc9a11059f3ca07e4582d91e509d09a60ea8a6d
Frang84 <skorniak.skorniak@wp.pl>
2024-12-18 15:43:46 +0100

    login
    login screen, basic validation

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{
-        headerShown: false
+        headerTitle: "Login"
       }}/>
       <Stack.Screen name="register" />
       <Stack.Screen name="(tabs)/Functions" options={{
--- front/app/index.tsx
+++ front/app/index.tsx
@@ -1,19 +1,55 @@
-import { Text, View, StyleSheet, StatusBar, SafeAreaView, TextInput } from "react-native"
+import { Text, View, StyleSheet, StatusBar, SafeAreaView, TextInput, Button, KeyboardAvoidingView } from "react-native"
+import { useState } from "react"
 import {Link} from "expo-router"
 
 
 const  LoginPage = () =>{
+  const [email, setEmail] = useState("") 
+  const [password, setPassword] = useState("") 
+  const [errors, setError] = useState({})
+
+  const validateForm = () =>{
+    let errors = {};
+    if (!email) errors.email = 'email is requierd';
+    if (!password) errors.password = 'password is requierd';
+    setError(errors);
+    return Object.keys(errors).length === 0;
+  }
+  const handleSubmit = () =>{
+    if(validateForm()){
+      console.log("Submitted", email, password);
+      setEmail("");
+      setPassword("");
+      setError({});
+      
+    }
+  }
+
     return(
-        <SafeAreaView style={styles.container}>
-            <Text>
-                Login Page
-            </Text>
-            <TextInput style={styles.input}></TextInput>
-             <Link href="/Functions/add">login</Link> 
-           
-            
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <SafeAreaView >
+            <Text >Email</Text>
+            {
+              errors.email ? (<Text style={styles.errorText}>{errors.email}</Text>) : null
+            }
+            <TextInput style={styles.input} 
+            placeholder="Enter your email" 
+            value={email} 
+            onChangeText={setEmail}/>
+
+            <Text>Password</Text>
+            {
+              errors.password ? (<Text style={styles.errorText}>{errors.password}</Text>) : null
+            }
+            <TextInput style={styles.input} 
+            placeholder="Enter your password"
+            secureTextEntry 
+            value={password} 
+            onChangeText={setPassword}/>
+            <Button title="Login" onPress={handleSubmit}></Button>
         </SafeAreaView>
-    )
+      </KeyboardAvoidingView>
+    )  
 }
 export default LoginPage;
 
@@ -27,9 +63,13 @@ const styles = StyleSheet.create({
   },
   input:{
     height: 40,
-    width: 80,
+    width: 160,
     margin: 12,
     padding: 10,
     borderWidth: 1
+  },
+  errorText:{
+    color: 'red',
+    marginBottom: 10,
   }
 });
